26bacf770cf27800c431a92b618d83c5af7f5f81
MatanElitzur <matan.elitzur@gmail.com>
2016-01-31 20:53:34 +0200

    Add constantsto filters

--- www/lottoGame/lottoConfigCtrl.js
+++ www/lottoGame/lottoConfigCtrl.js
@@ -1,19 +1,19 @@
 angular.module('lottoConfigModule')
-  .filter('displayLottoImages', function(){
+  .filter('displayLottoImages',['LOTTO_GAME_NAME', function(LOTTO_GAME_NAME){
         //// Create the return function and set the required parameter name to **input**
         return function(input){
             var out = [];
             // Using the angular.forEach method, go through the array of data and check if it's a lotto image
             angular.forEach(input, function(imageObject) {
               //'lotto' LOTTO_GAME_NAME
-              if(imageObject.addToGameObj.hasOwnProperty('lotto')){
+              if(imageObject.addToGameObj.hasOwnProperty(LOTTO_GAME_NAME)){
                 out.push(imageObject)
               }
             })
 
             return out;
         };
-  })
+  }])
   .controller('LottoConfigCtrl', ['ImageService' ,'$scope', function(ImageService, $scope) {
 
   var vm = this;
--- www/memoryGame/memoryConfigCtrl.js
+++ www/memoryGame/memoryConfigCtrl.js
@@ -1,18 +1,18 @@
 angular.module('memoryConfigModule')
-  .filter('displayMemoryImages', function(){
+  .filter('displayMemoryImages',['MEMORY_GAME_NAME', function(MEMORY_GAME_NAME){
     //// Create the return function and set the required parameter name to **input**
     return function(input){
       var out = [];
       // Using the angular.forEach method, go through the array of data and check if it's a lotto image
       angular.forEach(input, function(imageObject) {
-        if(imageObject.addToGameObj.hasOwnProperty('memory')){
+        if(imageObject.addToGameObj.hasOwnProperty(MEMORY_GAME_NAME)){
           out.push(imageObject)
         }
       })
 
       return out;
     };
-  })
+  }])
 
   .controller('MemoryConfigCtrl', ['$scope', 'ImageService', function($scope, ImageService){
   var vm = this;
