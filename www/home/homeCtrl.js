c37252b6f5b0c9ee9b28ac08b3632d1aa12e52b7
MatanElitzur <matan.elitzur@gmail.com>
2016-02-11 21:34:25 +0200

    Change home

--- www/home/homeCtrl.js
+++ www/home/homeCtrl.js
@@ -1,7 +1,34 @@
-angular.module('homeModule').controller('HomeCtrl',['$scope', '$state', function($scope, $state){
+angular.module('homeModule').controller('HomeCtrl',['$state', '$timeout', '$scope', function($state, $timeout, $scope){
+  var vm = this;
+  vm.animateMemoryGame = false;
+  vm.animateLottoGame = false;
+
+  vm.navigateMemory = function() {
+    vm.animateMemoryGame = true;
+    $timeout(function(){
+      $state.go('memory');
+    }, 2000);
+
+    $timeout(function(){
+      vm.animateMemoryGame = false;
+    }, 4000);
+  }
+
+  vm.navigateLotto = function() {
+    vm.animateLottoGame = true;
+    $timeout(function(){
+      $state.go('lotto');
+    }, 2000);
+
+    $timeout(function(){
+      vm.animateLottoGame = false;
+    }, 4000);
+  }
+  /*
 	    $scope.memoryImgPath = "home/img/memory.gif";
         $scope.lottoImgPath = "home/img/lotto.jpg";
 		$scope.test = function(){
 			$state.href();
 		};
+  */
 }]);
