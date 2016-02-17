85fa7c73f641a17f69d5672c532648c6e9a9960d
MatanElitzur <matan.elitzur@gmail.com>
2016-02-17 19:03:08 +0200

    Finish home page

--- www/home/homeCtrl.js
+++ www/home/homeCtrl.js
@@ -1,34 +1,27 @@
 angular.module('homeModule').controller('HomeCtrl',['$state', '$timeout', '$scope', function($state, $timeout, $scope){
   var vm = this;
-  vm.animateMemoryGame = false;
-  vm.animateLottoGame = false;
+  vm.animateHinge = false;
 
   vm.navigateMemory = function() {
-    vm.animateMemoryGame = true;
+    vm.animateHinge = true;
     $timeout(function(){
       $state.go('memory');
     }, 2000);
 
     $timeout(function(){
-      vm.animateMemoryGame = false;
+      vm.animateHinge = false;
     }, 4000);
   }
 
   vm.navigateLotto = function() {
-    vm.animateLottoGame = true;
+    vm.animateHinge = true;
     $timeout(function(){
       $state.go('lotto');
     }, 2000);
 
     $timeout(function(){
-      vm.animateLottoGame = false;
+      vm.animateHinge = false;
     }, 4000);
   }
-  /*
-	    $scope.memoryImgPath = "home/img/memory.gif";
-        $scope.lottoImgPath = "home/img/lotto.jpg";
-		$scope.test = function(){
-			$state.href();
-		};
-  */
+  
 }]);
